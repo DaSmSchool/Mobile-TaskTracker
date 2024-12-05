eeaabd3c22cc64823cb0fa3b985e3a298ad6f209
DaSmSchool <145377174+DaSmSchool@users.noreply.github.com>
2024-12-05 08:40:23 -0500

    add container layout progress

--- TaskTracker/app/src/main/java/com/example/TaskTracker/MainActivity.kt
+++ TaskTracker/app/src/main/java/com/example/TaskTracker/MainActivity.kt
@@ -3,18 +3,16 @@ package com.example.TaskTracker
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.AppBarLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val appBar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(appBar)
     }
 }
